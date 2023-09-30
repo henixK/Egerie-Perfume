56d5a8ecc7373d77fca188c2d6580488fc4664bb
Kevin Bautista <kevin.bautista@outlook.it>
2023-09-30 18:34:47 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Article title={"A Fragrance Inspired by the Legend of the Nymph Egeria"} text={"Step into a mesmerizing world of mythi- cal allure with Égérie, a captivating fragrance that pays homage to the legendary nymph Egeria. Drawing inspiration from the enchanting tales of ancient Rome, this perfume transports you to a lush, mystical realm where natu- re's beauty and a nymph's allure inter- twine in a dance of ethereal elegance."} button={<button className="my-10 uppercase relative">Learn more <span className="absolute border-2 border-black left-0 right-0 -bottom-1 "></span> </button>} />
       </Layout>
       <div className="w-screen mt-10">
-        <img className="w-full" src={modella} alt="" />
+        <img className="w-full h-full lg:h-screen" src={modella} alt="" />
       </div>
 
     </>
