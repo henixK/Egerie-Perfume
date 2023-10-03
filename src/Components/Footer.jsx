7c21413a942e2e17b260529ae48668f7f8539702
Kevin Bautista <kevin.bautista@outlook.it>
2023-10-03 13:20:20 +0200

    font size footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-lavanda text-lg text-beige py-4 px-20 lg:px-32 flex justify-between items-center gap-30 flex-col lg:flex-row">
+        <footer className="bg-lavanda text-base text-beige py-4 px-20 lg:px-32 flex justify-between items-center gap-30 flex-col lg:flex-row">
             <ul className="flex flex-col lg:flex-row text-center gap-4 lg:gap-10">
                 <p className="text-xl lg:hidden mb-2">Égérie Eau de Parfum</p>
                 <li>Home</li>
@@ -9,7 +9,7 @@ export default function Footer() {
                 <li>About</li>
             </ul>
             <div className="mt-10 lg:mt-0 px-18 ">
-            <p>&copy; 2023 Allison Salvador &amp; Evelina Maoctoe. All rights reserved.</p>
+            <p className="text-base">&copy; 2023 Allison Salvador &amp; Evelina Maoctoe. All rights reserved.</p>
             </div>
         </footer>
     )
